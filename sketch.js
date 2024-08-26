80693eb909143fa827ce6ae5ae4d51f34157fbdf
Going Obsolete <goingobsolete@gmail.com>
2024-08-26 09:53:58 +0200

    first commit

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+// const fs = require('fs');
+
+// // Step 1: Read the content of the rich text file
+// const filePath = 'path/to/your/file.rtf';
+// const content = fs.readFileSync(filePath, 'utf-8');
+
+// // Step 2: Use a regular expression to split the content into words, including punctuation
+// const wordsArray = content.match(/\S+/g);
+
+// // Step 3: Log the resulting array of words
+// console.log(wordsArray);
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(220);
+}
