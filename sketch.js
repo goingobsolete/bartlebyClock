ac5d345e87361df0fb0d83e94350ffe51362512e
Going Obsolete <goingobsolete@gmail.com>
2024-08-27 16:57:23 +0200

    stuck but i think the problem is time formatting

--- sketch.js
+++ sketch.js
@@ -1,45 +1,43 @@
 
 let wordsArray;
 let currentIndex = 0;
-let previousIndex = -1;
 let wordObj;
 let currentTime;
 
 function preload() {
-  wordsArray = loadJSON('data/words_with_line_numbers.json');
+  wordsArray = loadJSON('data/words_with_line_numbers.json', onJSONLoaded, onJSONError);
 } 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textFont('IBM Plex Mono');
+  textSize(32);
+  textAlign(CENTER, CENTER);
+
   currentTime = new Date().getTime();
+
   currentIndex = findInitialIndex(currentTime);
-  if (currentIndex == -1) {
-    wordObj = wordsArray[currentIndex];
-    previousIndex = currentIndex - 1;
-  }
+
 }
 
 function draw() {
   background('#F1F1F1');
   translate(width/2, height/2)
   fill(0);
+
   currentTime = new Date().getTime();
 
-  if (currentIndex < wordsArray.length && currentTime > wordsArray[currentIndex].endTime) {
-    currentIndex++;
+  if (currentIndex < wordsArray.length) {
+    if(currentTime > wordsArray[currentIndex].endTime) {
+      currentIndex++;
+    }
   }
 
   if (currentIndex < wordsArray.length) {
-    if (currentIndex != previousIndex) {
-      wordObj = wordsArray[currentIndex];
-      previousIndex = currentIndex;
-    }
-    
-    if (currentTime >= wordObj.startTime && currentTime <= wordObj.endTime) {
-    textSize(32);
-    textAlign(CENTER, CENTER);
-    text(wordObj.word,0,0)
+      let wordObj = wordsArray[currentIndex];
+      if (currentTime >= wordObj.startTime && currentTime <= wordObj.endTime) {
+        text(wordObj.word,0,0)
+      }
     }
   }
-}
+
