5eeb9e307078970e6d7ce9b475dd5b19f5eba0cf
Going Obsolete <goingobsolete@gmail.com>
2024-08-27 13:42:07 +0200

    abandoned individual weights for now. first hello world

--- sketch.js
+++ sketch.js
@@ -3,33 +3,23 @@ let nFont; // narrator font
 // let dFont; // dialogue font
 
 function preload() {
-  wordsArray = loadJSON(data/words_with_line_numbers.json, onJSONLoaded, onJSONLoadError);
-
-  // Load the font from Google Fonts
-  nFont = loadFont('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@100;200;300;400;500;600;700&display=swap');
+  wordsArray = loadJSON(data/words_with_line_numbers.json);
 } 
-
-function onJSONLoaded(data) {
-  wordsArray = data;
-  console.log('JSON loaded');
-}
-function onJSONError(error) {
-  console.error('Error loading JSON:', error);
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  textFont(nFont);
+  textFont('IBM Plex Mono');
 
 }
 
 function draw() {
-  background(255,0,0));
+  background('#F1F1F1');
   translate(width/2, height/2)
   fill(0);
+
   textSize(32);
   textAlign(CENTER, CENTER);
-  textStyle(NORMAL);
-  textFont('IBM Plex Mono', 200);
+  // textStyle(NORMAL);
   text('Hello, World!', 0, 0);
 
 }
