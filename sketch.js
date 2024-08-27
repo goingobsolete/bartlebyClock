1760669995b163f62c5ee96b1eae0d0404125d5f
Going Obsolete <goingobsolete@gmail.com>
2024-08-27 14:34:19 +0200

    trouble getting from hello world to variable

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
+
 let wordsArray;
-let nFont; // narrator font
+let currentIndex;
+let wordObj;
+
+// let nFont; // narrator font
 // let dFont; // dialogue font
 
 function preload() {
-  wordsArray = loadJSON(data/words_with_line_numbers.json);
+  wordsArray = loadJSON('data/words_with_line_numbers.json');
 } 
 
 function setup() {
@@ -17,9 +21,31 @@ function draw() {
   translate(width/2, height/2)
   fill(0);
 
+
   textSize(32);
   textAlign(CENTER, CENTER);
-  // textStyle(NORMAL);
-  text('Hello, World!', 0, 0);
+  text('Hello World',0,0)
 
+    // Check if wordsArray is loaded before accessing it
+  if (wordsArray) {
+    console.log(wordsArray[467].word);
+  } else {
+    console.log("wordsArray is not loaded yet.");
+
+  // let currentTime = new Date().getTime();
+
+  // if (currentTime < wordsArray[0].startTime) {
+  //   currentIndex = 0;
+  // }
+
+  // if (currentTime > wordsArray[currentIndex].endTime) {
+  //   currentIndex++;
+  // }
+
+  // if (currentIndex < wordsArray.length) {
+  //   let wordObj = wordsArray[currentIndex];
+  //   if (currentTime >= wordObj.startTime && currentTime <= wordObj.endTime) {
+  //     text(wordObj.word, 0, 0);
+  //   }
+  // }
 }
