a6bf3e4c131b685b1b0a351817350fcca7d97cca
Going Obsolete <goingobsolete@gmail.com>
2024-08-27 12:20:09 +0200

    added font and first draw

--- sketch.js
+++ sketch.js
@@ -1,20 +1,37 @@
+import { normalize } from "path/posix";
 
-// const fs = require('fs');
+let wordsArray;
+let nFont; // narrator font
+// let dFont; // dialogue font
 
-// // Step 1: Read the content of the rich text file
-// const filePath = 'path/to/your/file.rtf';
-// const content = fs.readFileSync(filePath, 'utf-8');
+function preload() {
+  wordsArray = loadJSON(data/words_with_line_numbers.json, onJSONLoaded, onJSONLoadError);
 
-// // Step 2: Use a regular expression to split the content into words, including punctuation
-// const wordsArray = content.match(/\S+/g);
+  // Load the font from Google Fonts
+  nFont = loadFont('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+} 
 
-// // Step 3: Log the resulting array of words
-// console.log(wordsArray);
+function onJSONLoaded(data) {
+  wordsArray = data;
+  console.log('JSON loaded');
+}
+function onJSONError(error) {
+  console.error('Error loading JSON:', error);
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(windowWidth, windowHeight);
+  textFont(nFont);
+
 }
 
 function draw() {
-  background(220);
+  background('#f1f1f1');
+  translate(width/2, height/2)
+  fill(0);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  textStyle(NORMAL);
+  textFont('IBM Plex Mono', 200);
+  text('Hello, World!', 0, 0);
+
 }
