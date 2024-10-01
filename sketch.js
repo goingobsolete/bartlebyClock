9fc180d448eb6816c00d4a0a507d73d28c57b743
goingobsolete <goingobsolete@gmail.com>
2024-10-01 12:04:45 +0200

    got rid of groups so that it would start working again

--- sketch.js
+++ sketch.js
@@ -2,52 +2,19 @@
 let wordsArray;
 let currentIndex;
 let wordObj;
-let group1, group2, group3, group4, group5;
-
-
 // let currentTime;
 
 function preload() {
-  loadJSON('data/words_with_line_numbers.json', onWordsLoaded, onJSONError);
-  loadJSON('data/group1.json', onGroup1Loaded, onJSONError);
-  loadJSON('data/group2.json', onGroup2Loaded, onJSONError);
-  loadJSON('data/group3.json', onGroup3Loaded, onJSONError);
-  loadJSON('data/group4.json', onGroup4Loaded, onJSONError);
-  loadJSON('data/group5.json', onGroup5Loaded, onJSONError);
+  wordsArray = loadJSON('data/words_with_line_numbers.json', onJSONLoaded, onJSONError);
 } 
 
-function onWordsLoaded(data) {
-  console.log('Words JSON Loaded');
+function onJSONLoaded(data) {
+  console.log('JSON Loaded');
   wordsArray = data;
 }
 
-function onGroup1Loaded(data) {
-  console.log('Group 1 JSON Loaded');
-  group1 = data;
-}
-
-function onGroup2Loaded(data) {
-  console.log('Group 2 JSON Loaded');
-  group2 = data;
-}
-
-function onGroup3Loaded(data) {
-  console.log('Group 3 JSON Loaded');
-  group3 = data;
-}
-
-function onGroup4Loaded(data) {
-  console.log('Group 4 JSON Loaded');
-  group4 = data;
-}
-
-function onGroup5Loaded(data) {
-  console.log('Group 5 JSON Loaded');
-  group5 = data;
-}
-
-function onJSONError(error) {
-  console.error('Error loading JSON:', error);
+function onJSONError() {
+  console.error('Failed to load JSON file.');
 }
 
 function setup() {
