d0ca4c25281046e7436a63eaed009dd4d7cc27d7
goingobsolete <goingobsolete@gmail.com>
2024-10-02 18:21:11 +0200

    added city groups to preload

--- dist/sketch.js
+++ dist/sketch.js
@@ -3,14 +3,41 @@
 var wordsArray;
 var currentIndex;
 var wordObj;
+var group1, group2, group3, group4, group5;
+
 // let currentTime;
 
 function preload() {
-  wordsArray = loadJSON('data/words_with_line_numbers.json', onJSONLoaded, onJSONError);
+  wordsArray = loadJSON('data/words_with_line_numbers.json', onWordJSONLoaded, onJSONError);
+  group1 = loadJSON('data/group_1.json', onGroup1JSONLoaded, onJSONError);
+  group2 = loadJSON('data/group_2.json', onGroup2JSONLoaded, onJSONError);
+  group3 = loadJSON('data/group_3.json', onGroup3JSONLoaded, onJSONError);
+  group4 = loadJSON('data/group_4.json', onGroup4JSONLoaded, onJSONError);
+  group5 = loadJSON('data/group_5.json', onGroup5JSONLoaded, onJSONError);
 }
-function onJSONLoaded(data) {
-  console.log('JSON Loaded');
+function onWordJSONLoaded(data) {
+  console.log('Word Array JSON Loaded');
   wordsArray = data;
+}
+function onGroup1JSONLoaded(data) {
+  console.log('Group 1 JSON Loaded');
+  group1 = data;
+}
+function onGroup2JSONLoaded(data) {
+  console.log('Group 2 JSON Loaded');
+  group2 = data;
+}
+function onGroup3JSONLoaded(data) {
+  console.log('Group 3 JSON Loaded');
+  group3 = data;
+}
+function onGroup4JSONLoaded(data) {
+  console.log('Group 4 JSON Loaded');
+  group4 = data;
+}
+function onGroup5JSONLoaded(data) {
+  console.log('Group 5 JSON Loaded');
+  group5 = data;
 }
 function onJSONError() {
   console.error('Failed to load JSON file.');
